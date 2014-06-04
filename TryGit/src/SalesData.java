a36f48784031d134b0d73ff5b22fe265a3930227
hassan 2 <mohassanns3@gmail.com>
2014-06-04 13:48:42 +0200

    Getting the sum value

--- TryGit/src/SalesData.java
+++ TryGit/src/SalesData.java
@@ -1,12 +1,17 @@
 
+/*
+ * Represents the sales data 
+ */
 public class SalesData {
 	int data [] = {0, 4, 42};
 	
 	public void display() {
+		int sum = 0;
 		for (int i=0; i<data.length;i++){
 			System.out.println("next value:"+ data[i]);
+			sum = sum +data[i];
 		}
-		
+		System.out.println("Sum is: "+sum);
 	}
 
 }
